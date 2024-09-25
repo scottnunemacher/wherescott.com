b50b0719624e1a7249172b25ac298f558b04f9db
Scott Nunemacher <29971822+scottnunemacher@users.noreply.github.com>
2024-09-25 08:43:34 -0700

    Reorganize src tree
    - _data inlcuding site.json
    - _includes including all dirs for eleventy.js
    - required a few placeholder .gitkeeps

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
 module.exports = function (eleventyConfig) {
+    // Allow passthrough copy
+    eleventyConfig.addPassthroughCopy("src/assets/fonts");
+    eleventyConfig.addPassthroughCopy("src/assets/images");
+    eleventyConfig.addPassthroughCopy("src/assets/js");
+
+    // Change 11ty defaults
     return {
         dir: {
             input: "src",
             output: "public",
+            data: "_data",
+            includes: "_includes",
+			layouts: "_includes/layouts",
         },
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
